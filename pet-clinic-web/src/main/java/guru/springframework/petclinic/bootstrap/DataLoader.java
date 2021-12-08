0c08c0982518acda531cfba7ae55c584a4b0d8e9
Sargis Sargsyan <sargiss987@gmail.com>
2021-12-08 21:23:45 +0400

    adding map implementation of data;

--- pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("John");
         owner1.setLastName("Doe");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Johnathan");
         owner2.setLastName("Pitcher");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loading owners ...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Ax");
         vet1.setLastName("Ax");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Sem");
         vet2.setLastName("Sem");
 
